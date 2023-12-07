88b3150d7a750df52223bca0379a5b5ff4024a2d
elies-billotta <elies.billotta@edu.univ-eiffel.fr>
2023-12-07 13:53:41 +0100

    Traitement des limites de la map

--- js/map.js
+++ js/map.js
@@ -1,24 +1,32 @@
-//define leaftlet map
-var map = L.map('map', {zoomControl:false}).setView([48.839623875988806, 2.588503717609941], 17);
+var map = L.map('map', { zoomControl: false }).setView([48.839623875988806, 2.588503717609941], 17);
 
-//define basemap
 var CartoDB_VoyagerNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	minZoom: 16,
-	maxZoom: 18,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    minZoom: 16,
+    maxZoom: 18,
 }).addTo(map);
 
-//limit map bounds
-var bounds = L.latLngBounds(
-  L.latLng(48.836, 2.572),
-  L.latLng(48.844, 2.595)
-);
 
+//limite de la map
+var bounds = L.latLngBounds(
+    L.latLng(48.836, 2.572),
+    L.latLng(48.844, 2.595)
+);
 map.setMaxBounds(bounds);
 
-// let cluster = L.markerClusterGroup();
-// cluster.addTo(map);
+// event qui désactive le le mouvement de la map si on est au niveau 16
+map.on('zoomend', function () {
+    if (map.getZoom() == 16) {
+        map.dragging.disable();
+    } else {
+        map.dragging.enable();
+    }
+});
 
-// function clearMarkers() {
-//   cluster.clearLayers();
-// }
+// event qui désactive le le mouvement de la map si on arrive aux limites de la map
+map.on('drag', function () {
+    var currentBounds = map.getBounds();
+    if (!bounds.contains(currentBounds)) {
+        map.panInsideBounds(bounds, { animate: false });
+    }
+});
