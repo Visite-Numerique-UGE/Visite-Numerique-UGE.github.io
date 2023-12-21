b3b0f4fdcbaca3b87b9ba42893bdf83399b2133a
2-1-1-2 <tanyan.francois@gmail.com>
2023-12-21 13:32:03 +0100

    YAY FUSION et dist marche

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     base: '',
+    optimizeDeps: {
+    esbuildOptions: {
+      target: "esnext",
+    },
+  },
+  build: {
+    target: "esnext",
+  },
 });
