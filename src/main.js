f60f26ca4b50811e5499f4b9af917794548f2679
2-1-1-2 <tanyan.francois@gmail.com>
2024-04-02 22:55:52 +0200

    Updates

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import './assets/main.scss';
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import 'leaflet/dist/leaflet.css';
-
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
--- src/views/CourseView.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import Parcours from "../components/CourseList.vue";
-</script>
-<template>
-  <Parcours />
-</template>
-
-<style></style>
