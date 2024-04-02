b1be1074c914c68b1f4ffd3444d9a8b8a6c3f196
2-1-1-2 <tanyan.francois@gmail.com>
2024-04-02 22:46:46 +0200

    Updates

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import HomePage from "../components/HomePage.vue";
+</script>
+
+<template>
+  <div class="_container">
+    
+    <div class="title"><h1>UGExplorer</h1></div>
+  </div>
+  <main>
+    <HomePage />
+  </main>
+</template>
+
+<script>
+export default {
+  name: "main",
+  components: {},
+  data() {},
+  methods: {},
+};
+</script>
+
+<style></style>
